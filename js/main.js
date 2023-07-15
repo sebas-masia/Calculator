f516513a4483b0697820478d2b31a87fd3a9cb40
sebas-masia <sebastianmasia@gmail.com>
2023-07-15 19:56:24 -0400

    refactor calculate

--- js/main.js
+++ js/main.js
@@ -1,17 +1,11 @@
 const calculate = (n1, operator, n2) => {
-    let result = '';
-    if (operator === 'add') {
-        result = parseFloat(n1) + parseFloat(n2);
-    } else if  (operator === 'subtract') {
-        result = parseFloat(n1) - parseFloat(n2);
-    } else if (operator === 'multiply') {
-        result = parseFloat(n1) * parseFloat(n2);
-    } else if  (operator === 'divide') {
-        result = parseFloat(n1)/parseFloat(n2);
+    const firstNum = parseFloat(n1)
+    const secondNum = parseFloat(n2)
+    if (operator === 'add') return firstNum + secondNum
+    if  (operator === 'subtract') return firstNum - secondNum
+    if (operator === 'multiply') return firstNum * secondNum
+    if  (operator === 'divide') return firstNum / secondNum
     }
-
-    return result
-}
 
 const calculator = document.querySelector('.calculator');
 const keys = calculator.querySelector('.calculator__keys');
